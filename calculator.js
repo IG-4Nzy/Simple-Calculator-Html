58bd3c5d2cc90ff9b24c77ab6b106725a960438f
IG-4Nzy <athulkrishnanas321@gmail.com>
2024-05-04 14:38:37 +0530

    .

--- calculator.js
+++ calculator.js
@@ -8,6 +8,7 @@ var secondNum = '';
 var length = '';
 
 
+
 function calculate(value) {
     if (value == '1' || value == '2' || value == '3' || value == '4' || value == '5' || value == '6' || value == '7' || value == '8' || value == '9' || value == '0') {
         if (result != '0') {
@@ -15,7 +16,6 @@ function calculate(value) {
                 newResult = result += value;
                 document.getElementById('result').value = newResult;
             } else {
-                console.log('result = ' + result);
                 newResult = result + value;
                 op = op2;
                 op2 = '';
@@ -28,60 +28,74 @@ function calculate(value) {
             result = newResult;
             document.getElementById('result').value = newResult;
         }
-    } else if (value == '+' || value == '-' || value == '*' || value == '/' || value == '%' || value == 'x') {
-        console.log('num1 = ' + firstNum);
-        console.log('num2 = ' + secondNum);
+    } else if (value == '+' || value == '-' || value == 'x' || value == '/' || value == '%') {
+        result = result.toString();
         length = result.length;
-        console.log('op = ' + op);
-        console.log('op2 = ' + op2);
-        console.log('length = ' + length);
-        console.log('result = ' + result);
-        if (length > 1) {
-            if (result.substring(length - 1) == '+') {
-                console.log('err');
-            } else if (op != '' || op2 != '') {
-                op2 = value;
-                var lengthOfResult = result.length;
-                var lengthOfFirstNum = firstNum.length;
-                secondNum = result.substring(lengthOfFirstNum + 1, lengthOfResult);
-                makeCalculation(firstNum, secondNum);
-            } else {
-                op = value;
-                firstNum = result;
-                newResult = result += value;
-                document.getElementById('result').value = newResult;
+        if (length >= 1) {
+            if (length > 1) {
+                if (result.substring(length - 1) == '+' || result.substring(length - 1) == '-' || result.substring(length - 1) == 'x' || result.substring(length - 1) == '/' || result.substring(length - 1) == '%' || result.substring(length - 1) == '.') {
+                    console.log('err');
+                } else if (op != '' || op2 != '') {
+                    op2 = value;
+                    var lengthOfResult = result.length;
+                    firstNum = firstNum.toString();
+                    var lengthOfFirstNum = firstNum.length;
+                    secondNum = result.substring(lengthOfFirstNum + 1, lengthOfResult);
+                    makeCalculation(firstNum, secondNum);
+                } else {
+                    op = value;
+                    firstNum = result;
+                    newResult = result += value;
+                    document.getElementById('result').value = newResult;
 
+                }
+            } else if (length == 1) {
+                if (result.substring(length - 1) == '+' || result.substring(length - 1) == '-' || result.substring(length - 1) == 'x' || result.substring(length - 1) == '/' || result.substring(length - 1) == '%' || result.substring(length - 1) == '.') {
+                    console.log('err');
+                }
+                else if (result == '0') {
+                    newResult = value;
+                    result = newResult;
+                    document.getElementById('result').value = newResult;
+                } else {
+                    op = value;
+                    firstNum = result;
+                    newResult = result += value;
+                    document.getElementById('result').value = newResult;
+                }
             }
-        } else if (result == '0') {
-            newResult = value;
-            result = newResult;
-            document.getElementById('result').value = newResult;
-        } else {
-            op = value;
-            firstNum = result;
-            newResult = result += value;
-            document.getElementById('result').value = newResult;
         }
+
     } else if (value == '+/-') {
         op = value;
         makeCalculation()
+        op = '';
     }
     else if (value == 'ac') {
-        result = '';
-        newResult = '0';
         op = '';
+        newResult = '0';
         op2 = '';
         firstNum = '';
         secondNum = '';
+        result = '0';
         document.getElementById('result').value = newResult;
+    } else if (value == '.') {
+        length = result.length;
+        if (result.substring(length - 1) == '.') {
+            console.log('err');
+        } else {
+            dec = true;
+            newResult = result + value;
+            result = newResult;
+            document.getElementById('result').value = newResult;
+        }
+
     } else if (value == '=') {
         if (op != '') {
+            firstNum = firstNum.toString();
             var lengthOfResult = result.length;
             var lengthOfFirstNum = firstNum.length;
             secondNum = result.substring(lengthOfFirstNum + 1, lengthOfResult);
-            console.log('first number = ' + firstNum);
-            console.log('second number = ' + secondNum);
-            console.log('result = ' + result);
             makeCalculation(firstNum, secondNum);
         }
     }
@@ -89,8 +103,14 @@ function calculate(value) {
 
 
 function makeCalculation(a, b) {
-    var num1 = parseInt(a);
-    var num2 = parseInt(b);
+    if (result.includes('.')) {
+        var num1 = parseFloat(a);
+        var num2 = parseFloat(b);
+    } else {
+        var num1 = parseInt(a);
+        var num2 = parseInt(b);
+    }
+
     switch (op) {
         case '+':
             newResult = num1 + num2;
@@ -144,12 +164,10 @@ function makeCalculation(a, b) {
             break;
         case '+/-':
             newResult = parseInt(result);
-            console.log(newResult);
             newResult = -newResult;
             result = newResult.toString();
             result = newResult;
             document.getElementById('result').value = newResult;
-            op = '';
             break;
     }
 }
